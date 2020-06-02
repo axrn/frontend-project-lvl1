683f139d9202610fd37bc942e70b374e8845740b
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-02 11:50:43 +0300

    replace switch/state

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function startGame(gameConfig) {
   const username = askNameAndGreet();
   showToUser(descriptionText);
   const roundsToWin = 3;
-  let state = 'USER_WIN';
+  let userWin = true;
   for (let round = 0; round < roundsToWin; round += 1) {
     const [question, correctAnswer] = getQuestionAndAnswer();
     const userAnswer = askUser(question);
@@ -22,19 +22,14 @@ function startGame(gameConfig) {
       showToUser('Correct!');
     } else {
       showToUser(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-      state = 'USER_LOOSE';
+      userWin = false;
       break;
     }
   }
-  switch (state) {
-    case 'USER_WIN':
-      showToUser(`Congratulations, ${username}!`);
-      break;
-    case 'USER_LOOSE':
-      showToUser(`Let's try again, ${username}!`);
-      break;
-    default:
-      throw new Error(`Unexpected state '${state}'`);
+  if (userWin) {
+    showToUser(`Congratulations, ${username}!`);
+  } else {
+    showToUser(`Let's try again, ${username}!`);
   }
 }
 
