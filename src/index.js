aa01865952e633867cb0052ce98ada8b4dc9df0c
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-08 14:48:41 +0300

    refactor game engine and delete "cli.js"

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import { askUser, showToUser } from './lib/cli.js';
+import readlineSync from 'readline-sync';
 
 
 export default (gameConfig) => {
   const [description, getQnA] = gameConfig();
-  showToUser('Welcome to the Brain Games!');
-  const username = askUser('May I have your name? ');
-  showToUser(`Hello, ${username}!`);
-  showToUser(description);
+  console.log('Welcome to the Brain Games!');
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+  console.log(description);
   const roundsToWin = 3;
   let userWin = true;
   for (let round = 0; round < roundsToWin; round += 1) {
     const [question, correctAnswer] = getQnA();
-    const userAnswer = askUser(question);
+    const userAnswer = readlineSync.question(question);
     if (String(userAnswer) === String(correctAnswer)) {
-      showToUser('Correct!');
+      console.log('Correct!');
     } else {
-      showToUser(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
       userWin = false;
       break;
     }
   }
   if (userWin) {
-    showToUser(`Congratulations, ${username}!`);
+    console.log(`Congratulations, ${username}!`);
   } else {
-    showToUser(`Let's try again, ${username}!`);
+    console.log(`Let's try again, ${username}!`);
   }
 };
