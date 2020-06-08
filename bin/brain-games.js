0aa5caad3b01ac9f97ba004f22318b64d06cd8d6
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-08 11:14:39 +0300

    delete useless file/command/field

--- bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { showToUser, askUser } from '../src/lib/cli.js';
-
-
-showToUser('Welcome to the Brain Games!');
-const username = askUser('May I have your name? ');
-showToUser(`Hello, ${username}!`);
