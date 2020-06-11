ece21ffd9df8e1bab4898481fa91c49ccbfbc32f
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-11 23:56:25 +0300

    refactor to built-in

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,8 +6,8 @@ const min = 1;
 const max = 100;
 
 const getGreatestCommonDivisor = (a, b) => {
-  const least = a < b ? a : b;
-  const most = a > b ? a : b;
+  const least = Math.min(a, b);
+  const most = Math.max(a, b);
   for (let i = least; i > 0; i -= 1) {
     if (least % i === 0 && most % i === 0) return i;
   }
