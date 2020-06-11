c8ac5eefc169852b9a486d2e4235f2951ffaca1a
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-12 00:43:47 +0300

    add handling zeros

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,6 +6,11 @@ const min = 1;
 const max = 100;
 
 const getGreatestCommonDivisor = (a, b) => {
+  if (a === 0 && b === 0) {
+    throw new Error('GCD(0, 0) is not defined!');
+  }
+  if (a === 0) return b;
+  if (b === 0) return a;
   const least = Math.min(a, b);
   const most = Math.max(a, b);
   for (let i = least; i > 0; i -= 1) {
