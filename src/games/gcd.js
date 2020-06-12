7e852daed871089ce477d04f1158eb88189d4c04
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-12 10:49:33 +0300

    add handling negative numbers

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,13 +6,15 @@ const min = 1;
 const max = 100;
 
 const getGreatestCommonDivisor = (a, b) => {
-  if (a === 0 && b === 0) {
+  const absA = Math.abs(a);
+  const absB = Math.abs(b);
+  if (absA === 0 && absB === 0) {
     throw new Error('GCD(0, 0) is not defined!');
   }
-  if (a === 0) return b;
-  if (b === 0) return a;
-  const least = Math.min(a, b);
-  const most = Math.max(a, b);
+  if (absA === 0) return absB;
+  if (absB === 0) return absA;
+  const least = Math.min(absA, absB);
+  const most = Math.max(absA, absB);
   for (let i = least; i > 0; i -= 1) {
     if (least % i === 0 && most % i === 0) return i;
   }
