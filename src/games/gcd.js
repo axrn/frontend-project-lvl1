7de2499c0c1b2901987fa847fd89ef5e87e4533e
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-16 13:39:47 +0300

    simplify gcd-function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,19 +4,13 @@ import { getRandomInt } from '../lib/utils.js';
 const description = 'Find the greatest common divisor of given numbers.';
 
 const getGreatestCommonDivisor = (a, b) => {
-  const absA = Math.abs(a);
-  const absB = Math.abs(b);
-  if (absA === 0 && absB === 0) {
+  if (a === 0 && b === 0) {
     throw new Error('GCD(0, 0) is not defined!');
   }
-  if (absA === 0) return absB;
-  if (absB === 0) return absA;
-  const least = Math.min(absA, absB);
-  const most = Math.max(absA, absB);
-  for (let divisor = least; divisor > 0; divisor -= 1) {
-    if (least % divisor === 0 && most % divisor === 0) return divisor;
+  for (let divisor = Math.abs(a); divisor > 0; divisor -= 1) {
+    if (a % divisor === 0 && b % divisor === 0) return divisor;
   }
-  return 1;
+  return Math.abs(b);
 };
 
 const getQuestionAndAnswer = () => {
