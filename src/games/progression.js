bc0d878d7d51d6df5198cae319d084b3b78ffe4f
Alexey Ryabokon <blank.basic@gmail.com>
2020-06-16 12:20:58 +0300

    simplify getProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,21 +6,19 @@ const lengthOfProgression = 10;
 const placeholder = '..';
 const separator = ' ';
 
-const getProgression = (start, step, length) => {
-  if (length < 1) return [];
-  const progression = [start];
-  while (progression.length < length) {
-    const prev = progression[progression.length - 1];
-    const current = prev + step;
+const getProgression = (firstElement, step, length) => {
+  const progression = [];
+  for (let index = 0; index < length; index += 1) {
+    const current = firstElement + step * index;
     progression.push(current);
   }
   return progression;
 };
 
 const getQuestionAndAnswer = () => {
-  const start = getRandomInt(1, 10);
+  const firstElement = getRandomInt(1, 10);
   const step = getRandomInt(1, 10);
-  const progression = getProgression(start, step, lengthOfProgression);
+  const progression = getProgression(firstElement, step, lengthOfProgression);
   const missingIndex = getRandomInt(0, progression.length - 1);
   const missing = progression.splice(missingIndex, 1, placeholder);
   const answer = String(missing);
